39f108fb4413b899d394e4eb7b84c890429b5d24
Francis <hello@francistries.science>
2026-09-21 11:16:45 +0800

    fix(web): restore desktop footer ui (#216)

--- apps/web/components/pages/home/_section/footer.tsx
+++ apps/web/components/pages/home/_section/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export function Footer() {
   return (
-    <footer className="text-muted-foreground px-2 text-center text-xs uppercase lg:text-left">
+    <footer className="text-muted-foreground px-2 text-xs uppercase">
       <p>
         © 2026 Francis Ignacio +{" "}
         <Link
