476abcf9553bb8d7d9b88233f2b791f9d7e1971a
Francis <hello@francistries.science>
2026-09-21 09:05:59 +0800

    fix(web): match work tab row height (#199)

--- apps/web/components/pages/home/_tabs/home-tabs.tsx
+++ apps/web/components/pages/home/_tabs/home-tabs.tsx
@@ -1,7 +1,7 @@
 export function HomeTabs() {
   return (
     <button
-      className="text-foreground block w-full p-2 text-left text-xs uppercase"
+      className="text-foreground flex h-8 w-full items-center p-2 text-left text-xs uppercase"
       type="button"
       role="tab"
       aria-selected={true}
